18000d1b2e170ad3a00f525a8f2d75d769b22f5a
anton-gandonio <for.site.joins@gmail.com>
2024-06-23 03:27:40 +0500

    god save me....

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlides() {
+    slides.forEach((slide, index) => {
+        slide.style.display = (index === slideIndex) ? 'block' : 'none';
+    });
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    showSlides();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlides();
+});
